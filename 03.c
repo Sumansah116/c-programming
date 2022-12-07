6d1a57f6292c39f1df83e1794ad54f5cc7324152
Sumansah116 <119616847+Sumansah116@users.noreply.github.com>
2022-12-07 12:26:38 +0530

    Add files via upload

---
+++ 03.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  	int num1,num2,tot;
+  	printf("Enter 2 numbers");
+  	scanf("%d%d",&num1,&num2);
+  	tot=num1+num2;
+  	printf("the sum is %d",tot);
+  	return 0;
+}
+
+
+
+
